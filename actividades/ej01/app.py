9cb02941e1a04baab1bcbbe4fe74fd1aa1b3b16c
abrilcontreras <83588570+abrilcontreras@users.noreply.github.com>
2024-09-12 13:40:36 -0300

    feat: app.py

--- actividades/ej01/app.py
+++ actividades/ej01/app.py
@@ -1,1 +1,13 @@
-# Aplicacion del servidor
+from microdot import send_file
+from microdot import Microdot
+
+app = Microdot()
+
+@app.get('/')
+async def index(request):
+    return send_file('/static/index.html')
+@app.route("/<dir>/<file>)
+async def static(request, dirt, file):
+    return send_file ("/"+dir+"/file)
+
+app.run(port = 80)
